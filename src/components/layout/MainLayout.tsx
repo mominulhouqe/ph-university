aa6b946f6357d723662089b41d5cd8a4e8e55d32
mominul <ulhouqemomin39@gmail.com>
2024-03-17 16:13:11 +0600

    first commit

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,88 @@
+import type { MenuProps } from "antd";
+import { Layout, Menu } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "3",
+    label: "User Management",
+    children: [
+      {
+        key: "a1",
+        label: "Create Admin",
+      },
+      {
+        key: "a2",
+        label: "Create Student",
+      },
+    ],
+  },
+];
+
+const MainLayout = () => {
+  return (
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            textAlign: "center",
+            height:"4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>PH Uni</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout style={{ marginLeft: 200, height: "100vh" }}>
+        <Header style={{ padding: 0 }} />
+        <Content
+          style={{
+            margin: "24px 16px 0",
+            overflow: "initial",
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              textAlign: "center",
+            }}
+          >
+            <h1>Main content goes to here</h1>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
